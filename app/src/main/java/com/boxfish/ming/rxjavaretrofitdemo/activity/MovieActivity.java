f243361b79a1fa9f882b6cb90fd81c05392c7b9d
maoaixianyu <2418>
2016-03-23 22:07:56 +0800

    add project

--- app/src/main/java/com/boxfish/ming/rxjavaretrofitdemo/activity/MovieActivity.java
+++ app/src/main/java/com/boxfish/ming/rxjavaretrofitdemo/activity/MovieActivity.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.boxfish.ming.rxjavaretrofitdemo.R;
+import com.boxfish.ming.rxjavaretrofitdemo.adapter.MovieAdapter;
+import com.boxfish.ming.rxjavaretrofitdemo.entity.MovieEntity;
+import com.boxfish.ming.rxjavaretrofitdemo.http.HttpUtils;
 
 import butterknife.Bind;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class MovieActivity extends AppCompatActivity {
+public class MovieActivity extends AppCompatActivity implements Callback<MovieEntity> {
 
 
+    private static final String TAG = "MovieActivity";
     @Bind(R.id.recycler)
     RecyclerView mRecyclerView;
+    private MovieAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,24 @@ public class MovieActivity extends AppCompatActivity {
         LinearLayoutManager manager = new LinearLayoutManager(this);
         manager.setOrientation(LinearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(manager);
+
+        mAdapter = new MovieAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+
+        Call<MovieEntity> call = HttpUtils.getService().getTopMovie(0,10);
+        call.enqueue(this);
+    }
+
+    @Override
+    public void onResponse(Call<MovieEntity> call, Response<MovieEntity> response) {
+
+        mAdapter.addAll(response.body().getSubjects());
+    }
+
+    @Override
+    public void onFailure(Call<MovieEntity> call, Throwable t) {
+
+        Log.e(TAG, "onFailure: == "+t.toString());
 
     }
 }
