68cf6dd3e99fdcae68c1459e549704fb6037d16a
maoaixianyu <2418>
2016-03-24 15:49:38 +0800

    添加ProgressDialog

---
+++ app/src/main/java/com/boxfish/ming/rxjavaretrofitdemo/progress/ProgressCancelListener.java
@@ -0,0 +1,13 @@
+package com.boxfish.ming.rxjavaretrofitdemo.progress;
+
+// +----------------------------------------------------------------------
+// | CreateTime: 16/3/24 
+// +----------------------------------------------------------------------
+// | Author:     xab(http://www.zhangkun.net.cn)
+// +----------------------------------------------------------------------
+// | CopyRight:  http://www.boxfish.cn
+// +----------------------------------------------------------------------
+public interface ProgressCancelListener {
+
+    void onCancelProgress();
+}
--- app/src/main/java/com/boxfish/ming/rxjavaretrofitdemo/subscribers/ProgressSubscriber.java
+++ app/src/main/java/com/boxfish/ming/rxjavaretrofitdemo/subscribers/ProgressSubscriber.java
@@ -3,6 +3,9 @@ package com.boxfish.ming.rxjavaretrofitdemo.subscribers;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.boxfish.ming.rxjavaretrofitdemo.progress.ProgressCancelListener;
+import com.boxfish.ming.rxjavaretrofitdemo.progress.ProgressDialogHandler;
+
 import rx.Subscriber;
 
 // +----------------------------------------------------------------------
@@ -19,25 +22,48 @@ import rx.Subscriber;
  * Observer本身是一个接口，他的特性是不管你怎么用，都不会解绑 Observer没有unsubscribe方法
  * 同时没有onStart方法 这个一会聊 所以使用 Subscriber
  * Subscriber给我们提供了onStart、onNext、onError、onCompleted四个方法
+ * <p/>
+ * <p/>
+ * 用于在Http请求开始时，自动显示一个ProgressDialog
+ * 在Http请求结束是，关闭ProgressDialog
+ * 调用者自己对请求数据进行处理
  *
  * @param <T>
  */
-public class ProgressSubscriber<T> extends Subscriber<T> {
+public class ProgressSubscriber<T> extends Subscriber<T> implements ProgressCancelListener {
 
     private SubscriberOnNextListener mSubscriberOnNextListener;
     private Context mContext;
 
+    // 添加消息处理
+    private ProgressDialogHandler mProgressDialogHandler;
+
     public ProgressSubscriber(SubscriberOnNextListener subscriberOnNextListener, Context context) {
         mSubscriberOnNextListener = subscriberOnNextListener;
         mContext = context;
+        mProgressDialogHandler = new ProgressDialogHandler(mContext, this, true);
+    }
+
+
+    private void showProgressDialog() {
+        if (mProgressDialogHandler != null) {
+            mProgressDialogHandler.obtainMessage(ProgressDialogHandler.SHOW_PROGRESS_DIALOG).sendToTarget();
+        }
+    }
+
+    private void dismissProgressDialog() {
+        if (mProgressDialogHandler != null) {
+            mProgressDialogHandler.obtainMessage(ProgressDialogHandler.DISMISS_PROGRESS_DIALOG).sendToTarget();
+            mProgressDialogHandler = null;
+        }
     }
 
     /**
-     * onStart方法我们用来启动一个ProgressDialog
+     * onStart方法我们用来启动一个ProgressDialog 展示
      */
     @Override
     public void onStart() {
-        super.onStart();
+        showProgressDialog();
     }
 
     /**
@@ -45,25 +71,36 @@ public class ProgressSubscriber<T> extends Subscriber<T> {
      */
     @Override
     public void onCompleted() {
-
+        dismissProgressDialog();
         Toast.makeText(mContext, "Get Top Movie Completed", Toast.LENGTH_SHORT).show();
 
     }
 
     /**
      * onError方法我们集中处理错误，同时也停止ProgressDialog
+     *
      * @param e
      */
     @Override
     public void onError(Throwable e) {
-
+        dismissProgressDialog();
         Toast.makeText(mContext, "error:" + e.getMessage(), Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     public void onNext(T t) {
-
         mSubscriberOnNextListener.onNext(t);
+    }
+
+    /**
+     * 取消订阅
+     */
+    @Override
+    public void onCancelProgress() {
+
+        if (!this.isUnsubscribed()){
+            this.unsubscribe();
+        }
     }
 }
